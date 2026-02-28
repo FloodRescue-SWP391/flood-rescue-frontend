4cd936ee90ce27f81bc7343e51efa25aab3d38b9
Trương Trần Anh Minh <minhtruong300605@gmail.com>
2026-02-28 16:02:26 +0700

    feat(manager): integrate category API into ManagerDashboard dropdown

---
+++ src/services/categoryService.js
@@ -0,0 +1,38 @@
+import { API_BASE_URL,fetchWithAuth } from "./apiClient";
+
+const BASE = `${API_BASE_URL}/Categories`;
+
+export const getAllCategories = {
+    async getAll(){
+        const res = await fetchWithAuth(`${BASE}`);
+        const data = await res.json()
+        return data;//APIResponse<List<CategoryDTO>>
+    },
+
+    async getById(id){
+        const res = await fetchWithAuth(`${BASE}/${id}`);
+        return await res.json();
+    },
+
+    async create(payload){
+        const res = await fetchWithAuth(`${BASE}`,{
+            method: "POST",
+            body: JSON.stringify(payload),
+        });
+        return await res.json();
+    },
+
+    async update(id, payload){
+        const res = await fetchWithAuth(`${BASE}/${id}`,{
+            method: "PUT",
+            body: JSON.stringify(payload),
+        });
+        return await res.json();
+    },
+    async remove(id){
+        const res = await fetchWithAuth(`${BASE}/${id}`,{
+            method: "DELETE",
+        }); 
+        return await res.json();
+    }
+}   
